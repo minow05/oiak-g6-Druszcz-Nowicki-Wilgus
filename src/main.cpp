eeab553140e3844aee14708f2ae6e61a2c288390
Jan Druszcz <jan.druszcz@onet.eu>
2025-06-17 19:10:18 +0200

    feat: add 8 bit flagged prefix adder + PPA

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,17 @@
-#include "../include/PrefixAdder8.hpp"
-#include "../include/FlaggedPrefixAdder8.hpp"
+#include "PrefixAdder8.hpp"
+#include "FlaggedPrefixAdder8.hpp"
 
 int main(){
-    uint8_t A = 0b10010001;
-    uint8_t B = 0b00010001;
+    uint8_t A = 0b00000010;
+    uint8_t B = 0b00100000;
     PrefixAdder8 adder(A, B);
     adder.printDebug();
 
-    // Przykład 1: Normalne dodawanie (bez flag)
-    FlaggedPrefixAdder8 adder1(A, B, 0b00000000);
-    std::cout << "bez flag:\n";
+    FlaggedPrefixAdder8 adder1(A, B);
     adder1.printDebug();
     std::cout << "\n";
-
-    // Przykład 2: Z flagami blokującymi carry
-    FlaggedPrefixAdder8 adder2(A, B, 0b00010000);
-    std::cout << "flaga na 4 bicie:\n";
+    
+    FlaggedPrefixAdder8 adder2(A, B, true, true);
     adder2.printDebug();
     std::cout << "\n";
 
